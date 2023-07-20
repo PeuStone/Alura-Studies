c075b0834fa8f9a11ad7848fd37d6df2ad3c509b
PeuStone <pedrohmotaaa@gmail.com>
2023-07-20 11:12:46 -0300

    Criação do formulario com label de texto e tempo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Botao from './componentes/Botao';
+
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
-      <Botao />
+      <Formulario />
     </div>
   );
 }
