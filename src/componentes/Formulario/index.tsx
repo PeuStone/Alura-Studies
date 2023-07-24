974cec9c2d770c69db83a6c19590a8524da171b4
PeuStone <pedrohmotaaa@gmail.com>
2023-07-24 09:43:52 -0300

    Instalação do SASS e estilização dos componentes

--- src/componentes/Formulario/index.tsx
+++ src/componentes/Formulario/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Botao from "../Botao";
+import './style.scss'
 
 class Formulario extends React.Component {
     render() {
         return (
-            <form>
-                <div>
+            <form className="novaTarefa">
+                <div className="inputContainer">
                     <label htmlFor="tarefa">
                         Adicione um estudo
                     </label>
@@ -15,7 +16,7 @@ class Formulario extends React.Component {
                         placeholder="O que você quer estudar?"
                         required />
                 </div>
-                <div>
+                <div className="inputContainer">
                     <label htmlFor="tempo">
                         Tempo
                     </label>
