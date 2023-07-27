f97373e54783d98c3546c272316e77f2baa534d5
PeuStone <pedrohmotaaa@gmail.com>
2023-07-27 10:54:06 -0300

    Implementação de conversor de tempo

--- src/componentes/Cronometro/index.tsx
+++ src/componentes/Cronometro/index.tsx
@@ -1,8 +1,11 @@
 import Botao from "../Botao";
 import { Relogio } from "./Relogio";
 import style from './Cronometro.module.scss'
+import { tempoParaSegundos } from "../../common/utils/date";
 
 export default function Cronometro() {
+    console.log('conversão: ', tempoParaSegundos('00:01:10'));
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}> Escolha um card e inicie o cronômetro</p>
--- src/componentes/Lista/Item/index.tsx
+++ src/componentes/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss'
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -15,7 +15,6 @@ export default function Item(
         selecionaTarefa
     }: Props) {
 
-    console.log('item atual:', { tarefa, tempo, selecionado, completado, id });
     return (
         <li
             className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
