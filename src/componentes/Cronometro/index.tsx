54fea8ad9fe23e624fb69f3f131247cf21ad4933
PeuStone <pedrohmotaaa@gmail.com>
2023-07-28 10:15:09 -0300

    Adicionando a captura de dados para o cronometro

--- src/componentes/Cronometro/index.tsx
+++ src/componentes/Cronometro/index.tsx
@@ -1,14 +1,29 @@
 import Botao from "../Botao";
 import { Relogio } from "./Relogio";
 import style from './Cronometro.module.scss'
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
-    console.log('conversão: ', tempoParaSegundos('00:01:10'));
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado]);
 
     return (
         <div className={style.cronometro}>
-            <p className={style.titulo}> Escolha um card e inicie o cronômetro</p>
+            <p className={style.titulo}>
+                Escolha um card e inicie o cronômetro
+            </p>
+            Tempo:{tempo}
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
